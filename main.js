19a87d09060a3ae2cac365a1418af2e0beed13c4
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-03 17:40:23 -0500

    Added dom accessibility to function

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log('Hello World!');
+//XHR stands for XML HTTP Request
+
+// printToDom = () => {
+//     selectedDiv = document.getElementById('')
+// };
+
+const domStringBuilder = (arrayToPrint) => {
+    console.log(arrayToPrint);
+}
+
+function executeThisCodeAfterFileLoads(){ //DON'T USE A FAT ARROW!
+    const data = JSON.parse(this.responseText);
+    domStringBuilder(data.rides);
+}
+
+function executeThisCodeIfXHRFails(){
+    console.error('oh shit');
+}
+
+const getRidesData = () => {
+    const myRequest = new XMLHttpRequest(); //makes a copy of the XHLHttpRequest in order to interact with server. You can retrieve data from a URL without having to do a full page refresh.
+    myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+    myRequest.addEventListener('error', executeThisCodeIfXHRFails);
+    myRequest.open('GET', './DB/rides.json');
+    myRequest.send();
+    console.log(myRequest);
+}
+
+const init = () => {
+    getRidesData();
+};
+
+init();
