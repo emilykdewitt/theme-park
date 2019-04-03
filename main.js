1dd8fdbd84177152f0d07fe4b1bf085978748c22
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-03 17:51:31 -0500

    WIP

--- main.js
+++ main.js
@@ -4,18 +4,21 @@
 //     selectedDiv = document.getElementById('')
 // };
 
+let rides = [];
+
 const domStringBuilder = (arrayToPrint) => {
     console.log(arrayToPrint);
-}
+};
 
 function executeThisCodeAfterFileLoads(){ //DON'T USE A FAT ARROW!
     const data = JSON.parse(this.responseText);
+    rides = data.rides;
     domStringBuilder(data.rides);
-}
+};
 
 function executeThisCodeIfXHRFails(){
     console.error('oh shit');
-}
+};
 
 const getRidesData = () => {
     const myRequest = new XMLHttpRequest(); //makes a copy of the XHLHttpRequest in order to interact with server. You can retrieve data from a URL without having to do a full page refresh.
@@ -24,7 +27,7 @@ const getRidesData = () => {
     myRequest.open('GET', './DB/rides.json');
     myRequest.send();
     console.log(myRequest);
-}
+};
 
 const init = () => {
     getRidesData();
